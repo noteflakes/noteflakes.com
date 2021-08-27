7e3d6c249adee6324a5939d50d19dd4c00b92802
Sharon Rosner <sharon@noteflakes.com>
2021-08-27 10:24:14 +0000

    Add Joni quote on home page

--- sites/noteflakes.com/pages/index.rb
+++ sites/noteflakes.com/pages/index.rb
@@ -2,6 +2,30 @@
 title: Noteflakes
 ---
 H {
+  quote = <<~EOF
+    And I dreamed I saw the bombers
+    Riding shotgun in the sky
+    And they were turning into butterflies
+    Above our nation
+
+    We are stardust
+    Billion year old carbon
+    We are golden
+    Caught in the devil's bargain
+    And we've got to get ourselves
+    back to the garden
+  EOF
+
+  article(class: 'quote_of_the_day') {
+    blockquote {
+      quote.lines.each { |l| span l; br }
+    }
+    p {
+      span '— '
+      a 'Joni Mitchell', href: 'https://www.youtube.com/watch?v=cRjQCvfcXn0', target: '\blank'
+    }
+  }
+
   articles = context[:pages]
     .select(/^\/articles/)
     .sort_by { |page| page.attributes['date'] }
