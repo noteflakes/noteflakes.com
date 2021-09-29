cea848e173458ca4f959abc311d9c9501d4b3d58
Sharon Rosner <sharon@noteflakes.com>
2021-09-29 07:25:03 +0000

    Tweak CSS

--- sites/noteflakes.com/_layouts/article.rb
+++ sites/noteflakes.com/_layouts/article.rb
@@ -4,7 +4,6 @@ H {
       title "Noteflakes: #{context[:page].title}"
       meta charset: 'utf-8'
       meta name: 'viewport', content: 'width=device-width, initial-scale=1.0'
-      meta name: 'referrer', content: 'no-referrer'
       style 'body { display: none }' # prevent FUOC
       link rel: 'icon', type: 'image/png', href: '/assets/nf-icon-black.png'
       link rel: 'stylesheet', type: 'text/css', href: '/assets/style.css'
