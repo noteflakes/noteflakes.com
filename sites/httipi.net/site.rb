a37bc0b115c70b519458368d8f64f2cf0e8b3cd8
Sharon Rosner <sharon@noteflakes.com>
2021-10-04 08:21:03 +0000

    Add httipi.net

--- sites/httipi.net/site.rb
+++ sites/httipi.net/site.rb
@@ -2,6 +2,60 @@
 
 export :call
 
+require 'rubyoshka'
+
+@layout = H {
+  html5 {
+    head {
+      title 'Tipi - a modern web server for Ruby'
+      meta charset: 'utf-8'
+      meta name: 'viewport', content: 'width=device-width, initial-scale=1.0'
+      # meta name: 'referrer', content: 'no-referrer'
+      # style 'body { display: none }' # prevent FUOC
+      # link rel: 'icon', type: 'image/png', href: '/assets/nf-icon-black.png'
+      link rel: 'stylesheet', type: 'text/css', href: '/assets/style.css'
+      # link rel: 'alternate', type: 'application/rss+xml', href: '/feeds/rss'
+    }
+    body {
+      header {
+        div(id: 'banner') {
+          emit "<p>#{'&nbsp;' * 7}ooo</p>"
+          emit "<p>#{'&nbsp;' * 5}oo</p>"
+          emit "<p>#{'&nbsp;' * 3}o</p>"
+          emit '<p>&nbsp;\\|/    <b>Tipi - a better web server for a better world</b></p>'
+          emit '<p>&nbsp;/ \\</p>'
+          emit '<p>/&nbsp;&nbsp;&nbsp;\\</p>'
+          hr()
+        }
+        ul {
+          li { a 'archive', href: '/archive' }
+          li { a 'about', href: '/about' }
+          li { a 'RSS feed', href: '/feeds/rss' }
+          li { a 'code', href: 'https://github.com/ciconia', target: '_blank' }
+        }
+      }
+      footer {
+        hr
+        p {
+          span 'Copyright © 2021 Sharon Rosner. This site runs on '
+          a 'Impression', href: 'https://github.com/digital-fabric/impression'
+          span ' and '
+          a 'Tipi', href: 'https://github.com/digital-fabric/tipi'
+          span '.'
+        }
+      }
+    }
+  }
+}
+
+def render_index
+  @layout.render
+end
+
+SITE_DIR = __dir__
+ASSETS_DIR = File.join(SITE_DIR, '_assets')
+
 def call(req)
-  req.respond('<h1>Tipi!!!!</h1>', 'Content-Type' => 'text/html')
+  req.on_root { req.respond(render_index, 'Content-Type' => 'text/html') }
+  req.on('assets') { req.serve_file(req.route_relative_path, base_path: ASSETS_DIR) }
 end
