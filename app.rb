783aaface1ac6001cc914412daf869616b301e44
Sharon Rosner <sharon@noteflakes.com>
2025-06-11 16:17:19 +0200

    Setup tolkora.net site

--- app.rb
+++ app.rb
@@ -4,7 +4,19 @@ require 'bundler/setup'
 require 'tp2'
 require 'modulation'
 
-TP2.config(&(import './sites/noteflakes.com/site.rb'))
+noteflakes  = import('./sites/noteflakes.com/site.rb')
+tolkora     = import('./sites/tolkora.net/site.rb')
+
+sites = {
+  'noteflakes.com'  => noteflakes,
+  'tolkora.net'     => tolkora,
+  'localhost:1234'  => tolkora
+}
+
+TP2.config do |req|
+  site = sites[req.host.downcase]
+  site ? site.(req) : respond(nil, ':status' => Qeweney::Status::BAD_REQUEST)
+end
 TP2.run()
 
 # Tipi.run_sites(
