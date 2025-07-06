e2461205ee1850729aae218425a0d6bec87f1f1b
Sharon Rosner <sharon@noteflakes.com>
2025-07-06 13:32:32 +0200

    Add missing host mappings

--- sites/_site.rb
+++ sites/_site.rb
@@ -1,3 +1,6 @@
 export route_by_host(
-  'localhost:1234' => 'noteflakes.com'
+  'localhost:1234' => 'noteflakes.com',
+  'localhost' => 'noteflakes.com',
+  'tolkora.org' => 'tolkora.net',
+  'tolkora.com' => 'tolkora.net'
 )
